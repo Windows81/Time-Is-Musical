69a0275329f212f1e540ed451c4d606aaacb774a
Windows81 <nastyahmede@gmail.com>
2022-02-19 06:37:56 +0000

    Added gradient interpolation and changed overal style!

--- .py
+++ .py
@@ -3,7 +3,7 @@ import time
 
 
 def gen(delay, audio, video, timestamp):
-    u = timestamp if timestamp is not None else time.time() + delay
+    u = int(timestamp if timestamp is not None else time.time() + delay)
     d = 10
     c = [
         [59],
@@ -27,7 +27,7 @@ def gen(delay, audio, video, timestamp):
             f"{''.join(f'amovie=minute_{v}0.wav,aloop=10:44100*60[mm{v}];'for v in m)}{''.join(f'amovie=hour_{v}.wav,aloop=60:44100*60[h{v}];'for v in h)}amovie=cycle_600s.wav,aloop=-1:44100*600[s600];amovie=cycle_60s.wav,aloop=-1:44100*60[s60];{''.join(f'amovie=cycle_60s_{i:02d}.wav,asplit={len(v)}'+''.join(f'[m{n}]'for n in v)+';'for i, v in enumerate(c))}{''.join(f'[mm{v}]'for v in m)}concat=v=0:a=1:n=6,aloop=-1:44100*60*60[m];{''.join(f'[m{v}]'for v in range(60))}concat=v=0:a=1:n=60,aloop=-1:44100*60*60[hh];{''.join(f'[h{v}]'for v in h)}concat=v=0:a=1:n=24,asplit={d}{''.join(f'[d{v}]'for v in range(d))};{''.join(f'[d{v}]'for v in range(d))}concat=v=0:a=1:n={d}[h];[hh][s60][m][h][s600]amix=inputs=5,volume=5,atrim={s},asetpts=PTS-STARTPTS"
             if audio
             else None,
-            f"gradients=size=1920x1080:x0=1:x1=1919:y0=1:y1=1079:r=7,drawtext=text='%{{pts\:localtime\:{ss}\:%H\\\\\:%M\\\\\:%S}}':fontsize=127:fontfile=./.ttf:box=1:boxborderw=23:x=(w-tw)/2:y=(h-lh)/2"
+            f"gradients=size=1920x1080:x0=1:x1=1919:y0=1:y1=1079:speed=0.0523598775598299:r=7[g0];gradients=size=1920x1080:x0=1919:x1=1:y0=1:y1=1079:speed=0.15707963267949:r=7[g1];[g0][g1]blend=all_mode=interpolate,drawtext=text='%{{pts\:localtime\:{ss}\:%H\\\\\:%M\\\\\:%S}}':fontsize=127:fontfile=./.ttf:box=1:boxborderw=30:boxcolor=#1f2123ff:fontcolor=white:x=(w-tw)/2:y=(h-lh)/2"
             if video
             else None,
         ],
