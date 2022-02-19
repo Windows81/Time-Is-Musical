093f41038d663c9efd4b726e1ae3dbcf9f0f5aff
Windows81 <nastyahmede@gmail.com>
2022-02-19 23:09:18 +0000

    Expanded on implementation in README; fixed memory leaks filling up RAM!

--- .py
+++ .py
@@ -17,33 +17,39 @@ def gen(delay, audio, video, timestamp):
         [0, 3, 8, 26, 32, 35, 49],
         [5, 19, 25, 39, 45, 50, 54],
     ]
-    s_h = [(v + u // 3600 + 1) % 24 for v in range(24)]
-    s_m0X = [(v + u // 60) % 60 for v in range(60)]
-    s_mX0 = [(v + u // 600) % 6 for v in range(6)]
+    s_h = [int((v + u // 3600 + 1) % 24) for v in range(24)]
+    s_m0X = [int((v + u // 60) % 60) for v in range(60)]
+    s_mX0 = [int((v + u // 600) % 6) for v in range(6)]
+    g_mX = ";".join(
+        f"amovie=cycle_s60_{i:02d}.wav,asplit={len(v)}{''.join(f'[m{n:02d}]' for n in v)}"
+        for i, v in enumerate(c)
+    )
     g_mmX = ";".join(f"amovie=minute_{v}0.wav,aloop=10:44100*60[mm{v}]" for v in s_mX0)
     g_hX = ";".join(f"amovie=hour_{v:02d}.wav,aloop=60:44100*60[h{v:02d}]" for v in s_h)
     g_s600 = f"amovie=cycle_s600.wav,aloop=-1:44100*600,atrim={u%600}[s600]"
     g_s60 = f"amovie=cycle_s60.wav,aloop=-1:44100*60,atrim={u%60}[s60]"
-    g_mX = ";".join(
-        f"amovie=cycle_s60_{i:02d}.wav,asplit={len(v)}"
-        + "".join(f"[m{n:02d}]" for n in v)
-        for i, v in enumerate(c)
-    )
     g_m = f"{''.join(f'[mm{v}]'for v in s_mX0)}concat=v=0:a=1:n=6,aloop=-1:44100*60*60,atrim={u%600}[m]"
     g_hh = f"{''.join(f'[m{v:02d}]'for v in s_m0X)}concat=v=0:a=1:n=60,aloop=-1:44100*60*60,atrim={u%60}[hh]"
-    g_hb = f"amovie=hour_{u//3600%24:02d}.wav,aloop={60-u//60%60}:44100*60,atrim={u%60}[hb]"
+    g_hb = f"amovie=hour_{int(u//3600%24):02d}.wav,aloop={int(60-u//60%60)}:44100*60,atrim={u%60}[hb]"
     g_dX = f"{''.join(f'[h{v:02d}]'for v in s_h)}concat=v=0:a=1:n=24,asplit={d}{''.join(f'[d{v}]'for v in range(d))}"
     g_h = f"[hb]{''.join(f'[d{v}]'for v in range(d))}concat=v=0:a=1:n={d+1}[h]"
-    ss = u % 86400
+    g_a = f"[hh][s600][s60][m][h]amix=inputs=5,volume=5,asetpts=PTS-STARTPTS"
+
+    g_g0 = f"gradients=size=1920x1080:x0=1:x1=1919:y0=1:y1=1079:speed=0.05:r=4[g0]"
+    g_g1 = f"gradients=size=1920x1080:x0=1919:x1=1:y0=1:y1=1079:speed=0.13:r=4[g1]"
+    g_v = (
+        f"[g0][g1]blend=all_mode=interpolate,drawtext=text='%{{pts\:localtime\:{(u+1/8)%86400}\:%H\\\\\:%M\\\\\:%S}}':"
+        + "fontsize=127:fontfile=./.ttf:box=1:boxborderw=30:boxcolor=#1f2123ff:fontcolor=white:x=(w-tw)/2:y=(h-lh)/2,"
+        + "setpts=PTS-STARTPTS"
+    )
+
     segs = filter(
         None,
         [
-            f"{g_mmX};{g_hX};{g_s600};{g_s60};{g_mX};{g_m};{g_hh};{g_hb};{g_dX};{g_h};[hh][s600][s60][m][h]amix=inputs=5,volume=5,asetpts=PTS-STARTPTS"
+            f"{g_mX};{g_mmX};{g_hX};{g_s600};{g_s60};{g_m};{g_hh};{g_hb};{g_dX};{g_h};{g_a}"
             if audio
             else None,
-            f"gradients=size=1920x1080:x0=1:x1=1919:y0=1:y1=1079:speed=0.0523598775598299:r=7[g0];gradients=size=1920x1080:x0=1919:x1=1:y0=1:y1=1079:speed=0.15707963267949:r=7[g1];[g0][g1]blend=all_mode=interpolate,drawtext=text='%{{pts\:localtime\:{ss}\:%H\\\\\:%M\\\\\:%S}}':fontsize=127:fontfile=./.ttf:box=1:boxborderw=30:boxcolor=#1f2123ff:fontcolor=white:x=(w-tw)/2:y=(h-lh)/2"
-            if video
-            else None,
+            f"{g_g0};{g_g1};{g_v}" if video else None,
         ],
     )
     return ";".join(f"{s}[out{i}]" for i, s in enumerate(segs))
--- _stream.sh
+++ _stream.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 f=$(python .py $@)
 echo $f
-ffmpeg -re -f lavfi -i "$f" -v error -stats -g 48 -pix_fmt yuv420p -x264-params keyint=48:min-keyint=48:scenecut=-1 -b:a 128k -ar 44100 -acodec aac -vcodec libx264 -preset superfast -bufsize 960k -crf 28 -threads 2 -f flv "rtmp://b.rtmp.youtube.com/live2/gvwr-wj12-3vcs-04uw-1vp7"
+ffmpeg -discard nokey -dn -f lavfi -i "$f" -v error -stats -g 69 -pix_fmt yuv420p -x264-params keyint=69:min-keyint=69:scenecut=-1 -g 69 -b:a 128k -ar 44100 -acodec aac -vcodec libx264 -preset superfast -bufsize 960k -crf 28 -threads 2 -dn -f flv "rtmp://b.rtmp.youtube.com/live2/gvwr-wj12-3vcs-04uw-1vp7"
