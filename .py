d75b6d9708feb4ae2671c267973552c278909719
Windows81 <nastyahmede@gmail.com>
2022-02-23 04:29:15 +0000

    Fixed 10s minute bug.

--- .py
+++ .py
@@ -17,23 +17,25 @@ def gen(delay, audio, video, timestamp):
         [0, 3, 8, 26, 32, 35, 49],
         [5, 19, 25, 39, 45, 50, 54],
     ]
-    s_h = [int((v + u // 3600 + 1) % 24) for v in range(24)]
-    s_m0X = [int((v + u // 60) % 60) for v in range(60)]
-    s_mX0 = [int((v + u // 600) % 6) for v in range(6)]
+    s_h = [(v + u // 3600 + 1) % 24 for v in range(24)]
+    s_m0X = [(v + u // 60) % 60 for v in range(60)]
+    s_mX0 = [(v + u // 600 + 1) % 6 for v in range(6)]
     g_mX = ";".join(
         f"amovie=cycle_s60_{i:02d}.wav,asplit={len(v)}{''.join(f'[m{n:02d}]' for n in v)}"
         for i, v in enumerate(c)
     )
-    g_mmX = ";".join(f"amovie=minute_{v}0.wav,aloop=10:44100*60[mm{v}]" for v in s_mX0)
-    g_hX = ";".join(f"amovie=hour_{v:02d}.wav,aloop=60:44100*60[h{v:02d}]" for v in s_h)
+    g_mmX = ";".join(f"amovie=minute_{v}0.wav,aloop=9:44100*60[m{v}X]" for v in s_mX0)
+    g_hX = ";".join(f"amovie=hour_{v:02d}.wav,aloop=59:44100*60[h{v:02d}]" for v in s_h)
     g_s600 = f"amovie=cycle_s600.wav,aloop=-1:44100*600,atrim={u%600}[s600]"
     g_s60 = f"amovie=cycle_s60.wav,aloop=-1:44100*60,atrim={u%60}[s60]"
-    g_m = f"{''.join(f'[mm{v}]'for v in s_mX0)}concat=v=0:a=1:n=6,aloop=-1:44100*60*60,atrim={u%600}[m]"
-    g_hh = f"{''.join(f'[m{v:02d}]'for v in s_m0X)}concat=v=0:a=1:n=60,aloop=-1:44100*60*60,atrim={u%60}[hh]"
-    g_hb = f"amovie=hour_{int(u//3600%24):02d}.wav,aloop={int(60-u//60%60)}:44100*60,atrim={u%60}[hb]"
+    g_m = f"{''.join(f'[m{v}X]'for v in s_mX0)}concat=v=0:a=1:n=6,aloop=-1:44100*60*60[mX0]"
+    g_mb = f"amovie=minute_{u//600%6}0.wav,aloop={9-u//60%10}:44100*60,atrim={u%60}[mb]"
+    g_mm = f"[mb][mX0]concat=v=0:a=1:n=2[m]"
+    g_hh = f"{''.join(f'[m{v:02d}]'for v in s_m0X)}concat=v=0:a=1:n=60,aloop=-1:44100*60*60,atrim={u%60}[m0X]"
+    g_hb = f"amovie=hour_{u//3600%24:02d}.wav,aloop={59-u//60%60}:44100*60,atrim={u%60}[hb]"
     g_dX = f"{''.join(f'[h{v:02d}]'for v in s_h)}concat=v=0:a=1:n=24,asplit={d}{''.join(f'[d{v}]'for v in range(d))}"
     g_h = f"[hb]{''.join(f'[d{v}]'for v in range(d))}concat=v=0:a=1:n={d+1}[h]"
-    g_a = f"[hh][s600][s60][m][h]amix=inputs=5,volume=5,asetpts=PTS-STARTPTS"
+    g_a = f"[m0X][s600][s60][m][h]amix=inputs=5,volume=5,asetpts=PTS-STARTPTS"
 
     g_g0 = f"gradients=size=1920x1080:x0=1:x1=1919:y0=1:y1=1079:speed=0.05:r=4[g0]"
     g_g1 = f"gradients=size=1920x1080:x0=1919:x1=1:y0=1:y1=1079:speed=0.13:r=4[g1]"
@@ -49,7 +51,7 @@ def gen(delay, audio, video, timestamp):
     segs = filter(
         None,
         [
-            f"{g_mX};{g_mmX};{g_hX};{g_s600};{g_s60};{g_m};{g_hh};{g_hb};{g_dX};{g_h};{g_a}"
+            f"{g_mX};{g_mmX};{g_hX};{g_s600};{g_s60};{g_m};{g_mb};{g_mm};{g_hh};{g_hb};{g_dX};{g_h};{g_a}"
             if audio
             else None,
             f"{g_g0};{g_g1};{g_v}" if video else None,
